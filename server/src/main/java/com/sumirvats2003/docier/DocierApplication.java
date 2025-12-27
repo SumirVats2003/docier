aef08890298cd80a0ac859bb5c668edc3fe4595c
Sumir Vats <sumirvats@gmail.com>
2025-12-27 15:21:30 +0530

    initialised backend project

---
+++ server/src/main/java/com/sumirvats2003/docier/DocierApplication.java
@@ -0,0 +1,13 @@
+package com.sumirvats2003.docier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocierApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/sumirvats2003/docier/DocierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumirvats2003.docier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
