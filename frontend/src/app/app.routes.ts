26905f8be71eb9d3bce3571a16b7d95c786f70fc
Anannya410 <anannya04hiteshi@gmail.com>
2025-12-29 20:42:56 +0530

    Added Home Component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   {
@@ -15,5 +16,9 @@ export const routes: Routes = [
   {
     path: 'signup',
     component: SignupComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
   }
 ];
--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   imports: [RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  constructor(private router: Router) {}
 
+  async onSubmit() {
+    this.router.navigate(['/home']);
+  }
 }
