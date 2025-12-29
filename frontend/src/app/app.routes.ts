31276c61f2d338e7d3b872c8c6e242b3382b9530
Anannya410 <anannya04hiteshi@gmail.com>
2025-12-29 19:02:23 +0530

    Created signup component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
   {
@@ -11,4 +12,8 @@ export const routes: Routes = [
     path: 'login',
     component: LoginComponent,
   },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  }
 ];
