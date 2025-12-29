6baf100dc0f980add068e0829f63d663dd9d590c
Anannya410 <anannya04hiteshi@gmail.com>
2025-12-29 20:40:29 +0530

    Added signup design

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+  constructor(private router:Router) {}
 
+  async onSubmit() {
+    this.router.navigate(['/home']);
+  }
 }
